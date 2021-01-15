93c9be1ba3225aa7c3250e9a1d82937d4a19e34e
Khalil Najjar <khn@alteos.com>
2021-01-15 17:25:12 +0100

    fox

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
          :confirmable, :omniauthable,
          omniauth_providers: %i[twitter]
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    end
+  end
+
   def has_connection_with(provider)
     auth = authorizations.where(provider: provider).first
     if auth.present?
