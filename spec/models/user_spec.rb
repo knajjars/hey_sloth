88662a5c98eb0b224385bcb4068f479a4194af5c
Khalil Najjar <knajjars@gmail.com>
2021-01-24 18:03:05 +0100

    finish user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'has email field' do
     user = User.new password: 'password', password_confirmation: 'password'
-
     expect(user).to_not be_valid
-
   end
 
   it 'has valid email field' do
     user = User.new email: "not_an_email", password: 'password', password_confirmation: 'password'
-
     expect(user).to_not be_valid
 
     user.email = "test@test.com"
@@ -22,16 +19,19 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  # it 'can have many testimonials' do
-  #
-  # end
+  it 'can have many testimonials' do
+    testimonials = User.reflect_on_association :testimonials
+    expect(testimonials.macro).to eq :has_many
+  end
 
-  # if 'can have many shareable_links' do
-  #  
-  # end
-  #
-  # it 'can have many authorizations' do
-  #  
-  # end
+  it 'can have many shareable_links' do
+    shareable_links = User.reflect_on_association :shareable_links
+    expect(shareable_links.macro).to eq :has_many
+  end
+
+  it 'can have many authorizations' do
+    authorizations = User.reflect_on_association :authorizations
+    expect(authorizations.macro).to eq :has_many
+  end
 
 end
