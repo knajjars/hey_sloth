e2186072dcf55f371cecff62a01b15b3ddd8b4eb
Khalil Najjar <khn@alteos.com>
2021-01-28 10:40:07 +0100

    pending tests for collects

---
+++ spec/requests/collect_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Collects", type: :request do
+  describe '#from_shareable_link_new' do
+    xit ''
+  end
+
+  describe '#from_shareable_link_create' do
+    xit ''
+  end
+
+  describe '#send_email_new' do
+    xit ''
+  end
+
+  describe '#send_email_create' do
+    xit ''
+  end
+
+  describe '#twitter_search' do
+    xit ''
+  end
+
+  describe '#twitter_post_new' do
+    xit ''
+  end
+
+  describe '#twitter_post_create' do
+    xit ''
+  end
+
+  describe '#delete_tweet' do
+    xit ''
+  end
+end
