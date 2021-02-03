1904ee55a7a4e65d515cf371c9d0bc7955d0d4c0
Khalil Najjar <khn@alteos.com>
2021-02-03 09:23:40 +0100

    update responsiveness for dashboard

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,7 +2,7 @@ import {Controller} from "stimulus"
 
 export default class extends Controller {
     static get targets() {
-        return ['profileOptions', 'profileButton']
+        return ['profileOptions', 'profileButton', 'mobileMenu', 'menuOpenIcon', 'menuCloseIcon']
     }
 
     static classes = ["hidden"]
@@ -15,7 +15,13 @@ export default class extends Controller {
         }
     }
 
-    openProfile() {
+    toggleProfile() {
         this.profileOptionsTarget.classList.toggle(this.hiddenClass)
+    }
+
+    toggleMobileMenu() {
+        this.mobileMenuTarget.classList.toggle(this.hiddenClass)
+        this.menuOpenIconTarget.classList.toggle(this.hiddenClass)
+        this.menuCloseIconTarget.classList.toggle(this.hiddenClass)
     }
 }
