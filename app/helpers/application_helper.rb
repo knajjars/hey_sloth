2ce0c2d4a51e4e5d297ded904a59b66692f1b237
Khalil Najjar <knajjars@gmail.com>
2021-02-06 18:13:43 +0100

    finish show testimonial for tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   def formatted_date(date, with_time_passed:)
     formatted_date = date.strftime('%B %d, %Y').to_s
-    formatted_date << "(#{time_ago_in_words date} ago)" if with_time_passed
+    formatted_date << " (#{time_ago_in_words date} ago)" if with_time_passed
     formatted_date
   end
 
@@ -19,7 +19,7 @@ module ApplicationHelper
   end
 
   def embed_tweet(t)
-    tweet = TwitterApi.client.oembed(t)
+    tweet = TwitterApi.client.oembed(t, hide_thread: true)
     raw(tweet.html)
   end
 end
--- app/helpers/collect_helper.rb
+++ app/helpers/collect_helper.rb
@@ -1,10 +1,8 @@
 module CollectHelper
   def tweets_timeline
     if current_user.twitter?
-      @tweets = current_user.twitter.mentions_timeline(tweet_mode: "extended")
-      unless @tweets.empty?
-        render 'tweet/collect_tweets'
-      end
+      @tweets = current_user.twitter.mentions_timeline(tweet_mode: 'extended')
+      render 'tweet/collect_tweets' unless @tweets.empty?
     else
       raw "<p>Please link your Twitter account in your #{link_to 'profile', edit_user_registration_path} first.</p>"
     end
