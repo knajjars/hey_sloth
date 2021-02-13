47ee9f7e53093ebc5d88f77b86e89c4693df9899
Khalil Najjar <khn@alteos.com>
2021-02-13 17:22:45 +0100

    add more styling and twitter link profile with helper

--- app/helpers/collect_helper.rb
+++ app/helpers/collect_helper.rb
@@ -4,7 +4,7 @@ module CollectHelper
       @tweets = current_user.twitter.mentions_timeline(tweet_mode: 'extended')
       render 'tweet/collect_tweets' unless @tweets.empty?
     else
-      raw "<p>Please link your Twitter account in your #{link_to 'profile', edit_user_registration_path} first.</p>"
+      raw "<p>Please link your Twitter account in your #{link_to 'profile', edit_user_registration_path("link_twitter": true)} first.</p>"
     end
   end
 end
