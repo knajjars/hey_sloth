a9d93812d80621956db5aa36670af8b35e72db57
Khalil Najjar <knajjars@gmail.com>
2021-02-18 17:09:30 +0100

    New dashboard (#9)
    * new dashboard
    
    * finish
    
    * final touches
    
    * finish refactor
    
    Co-authored-by: Khalil Najjar <khn@alteos.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @testimonials = current_user.testimonials
+    @pagy, @testimonials = pagy(current_user.testimonials.with_attached_image.order(created_at: :desc), items: 10)
+    @showcased_testimonials = current_user.testimonials.showcased.count
   end
 end
