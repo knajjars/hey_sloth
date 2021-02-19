9ab452e544cd8379b9157a29a6e7c77bd8e430be
Khalil Najjar <khn@alteos.com>
2021-02-19 10:25:03 +0100

    firelink concept

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @pagy, @testimonials = pagy(current_user.testimonials.with_attached_image.order(created_at: :desc), items: 10)
     @showcased_testimonials = current_user.testimonials.showcased.count
+    @tweet_testimonials = current_user.testimonials.tweets.count
   end
 end
