a412dd25f49fac2370d1cd0fae4e7fe67aed5e00
Khalil Najjar <khn@alteos.com>
2021-02-22 22:53:00 +0100

    fix webpack

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ gem 'omniauth-twitter'
 gem 'pundit', '~> 2.1'
 gem 'sib-api-v3-sdk', '~> 7.3'
 gem 'twitter', '~> 7.0'
-gem "tailwindcss-rails", "~> 0.3.1"
+gem "tailwindcss-rails", "~> 0.3.3"
 gem 'pagy', '~> 3.10'
 gem 'twitter-text', '~> 3.1'
 gem 'rack-cors', '~> 1.1', '>= 1.1.1'
--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
-module.exports = {
+let environment = {
   plugins: [
-  require('tailwindcss'),
-	  require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+    require("autoprefixer"),
+    require("tailwindcss")("./app/javascript/stylesheets/tailwind.config.js"),
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: "no-2009",
       },
-      stage: 3
-    })
-  ]
-}
+      stage: 3,
+    }),
+  ],
+};
+
+module.exports = environment;
