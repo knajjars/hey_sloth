0346eb107af75c3607b4fb16fc0c3f018c8a71b9
Khalil Najjar <khn@alteos.com>
2021-03-01 11:09:05 +0100

    fix code snippet

--- app/javascript/controllers/code_highlight_controller.js
+++ app/javascript/controllers/code_highlight_controller.js
@@ -1,15 +1,19 @@
 import {Controller} from "stimulus"
 
 import hljs from 'highlight.js/lib/core'
-import 'highlight.js/styles/github.css';
+import 'highlight.js/styles/shades-of-purple.css';
 
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 
 export default class extends Controller {
+    static targets = ['content', 'textarea']
 
     connect() {
         document.querySelectorAll('div.code-snippet pre code').forEach((block) => {
             hljs.highlightBlock(block);
         });
+
+        this.textareaTarget.value = this.contentTarget.textContent;
+
     }
 }
