2801f55c05c93f658626b2d8ae58f7d9d4d2b846
Khalil Najjar <khn@alteos.com>
2021-03-04 15:30:31 +0100

    update lading page

--- app/javascript/controllers/page_controller.js
+++ app/javascript/controllers/page_controller.js
@@ -2,6 +2,39 @@ import {Controller} from 'stimulus'
 
 export default class extends Controller {
     connect() {
+
+        const navMenuDiv = document.getElementById("nav-content");
+        const navMenu = document.getElementById("nav-toggle");
+
+        document.onclick = check;
+
+        function check(e) {
+            const target = (e && e.target) || (event && event.srcElement);
+
+            if (!checkParent(target, navMenuDiv)) {
+                if (checkParent(target, navMenu)) {
+                    if (navMenuDiv.classList.contains("hidden")) {
+                        navMenuDiv.classList.remove("hidden");
+                    } else {
+                        navMenuDiv.classList.add("hidden");
+                    }
+                } else {
+                    navMenuDiv.classList.add("hidden");
+                }
+            }
+        }
+
+        function checkParent(t, elm) {
+            while (t.parentNode) {
+                if (t === elm) {
+                    return true;
+                }
+                t = t.parentNode;
+            }
+            return false;
+        }
+
+
         const links = document.querySelectorAll("#header ul a");
         for (const link of links) {
             link.addEventListener("click", clickHandler);
